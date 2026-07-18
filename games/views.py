3d0d84adbf5d7a1447897bf038af594397ffa01c
PichuDestroyer <elvincheung0321@gmail.com>
2026-07-18 16:25:05 +0800

    Changed api to nba_api to improve efficiency

--- games/views.py
+++ games/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 import requests
 from datetime import date
+from nba_api.stats.endpoints import leaguegamefinder
+import pandas as pd
 
 def game_listing(request):
     auth_token = "26af8e03-7266-4878-a6ce-6890a733f7d5"
@@ -22,53 +24,61 @@ def game_listing(request):
     if start_date and end_date:
         submit = True
 
-        params = {
-            "cursor": 0,
-            "per_page": 100,
-            "start_date": start_date,
-            "end_date": end_date,
-        }
+        # params = {
+        #     "cursor": 0,
+        #     "per_page": 100,
+        #     "start_date": start_date,
+        #     "end_date": end_date,
+        # }
 
-        while True:
-            response = requests.get("https://api.balldontlie.io/v1/games", headers=headers, params=params)
-            if response.status_code != 200:
-                break
-            
-            data = response.json()
-            
-            for game in data["data"]:
-                home_team = game["home_team"]["full_name"]
-                visitor_team = game["visitor_team"]["full_name"]
-                if team:
-                    home_name = home_team.lower()
-                    visitor_name = visitor_team.lower()
-                    if team not in home_name and team not in visitor_name:
-                        continue
+        # while True:
+        game = leaguegamefinder.LeagueGameFinder(date_from_nullable=start_date, date_to_nullable=end_date)
 
-                home_score = game["home_team_score"]
-                visitor_score = game["visitor_team_score"]
+        # df = game.get_data_frames()[0]
+        # df2 = df.sort_values("GAME_ID").reset_index()   
+        
+        df1 = game.get_data_frames()[0].sort_values("GAME_ID").reset_index().drop("index", axis=1)
+        home_game = df1[df1["MATCHUP"].str.contains("@")].reset_index().drop("index", axis=1)
+        away_game = df1[-df1["MATCHUP"].str.contains("@")].reset_index().drop("index", axis=1)
+        data = pd.merge(home_game, away_game, on=["GAME_ID", "GAME_DATE", "SEASON_ID"], suffixes=["_H","_A"])
 
-                if home_score > visitor_score:
-                    winner = home_team
-                    wins[home_team] = wins.get(home_team, 0) + 1
-                elif home_score < visitor_score:
-                    winner = visitor_team
-                    wins[visitor_team] = wins.get(visitor_team, 0) + 1
-                else:
-                    winner = "Draw"
+        # data = data.drop("index", axis=1)
+        
+        for i, game in data.iterrows():
+            home_team = game["TEAM_NAME_H"]
+            home_ab = game["TEAM_ABBREVIATION_A"]
+            visitor_ab = game["TEAM_ABBREVIATION_H"]
+            visitor_team = game["TEAM_NAME_A"]
+            if team:
+                home_name = home_team.lower()
+                visitor_name = visitor_team.lower()
+                if team not in home_name and team not in visitor_name and team not in home_ab and team not in visitor_ab:
+                    continue
 
-                games.append({
-                    "date": game["date"],
-                    "home_team": home_team,
-                    "visitor_team": visitor_team,
-                    "visitor_score": visitor_score,
-                    "home_score": home_score,
-                    "winner": winner,
-                })
-            next_cursor = data.get("meta", {}).get("next_cursor")
-            params["cursor"] = next_cursor
-            if not next_cursor:
-                break
+            home_score = game["PTS_H"]
+            visitor_score = game["PTS_A"]
+
+            if home_score > visitor_score:
+                winner = home_team
+                wins[home_team] = wins.get(home_team, 0) + 1
+            elif home_score < visitor_score:
+                winner = visitor_team
+                wins[visitor_team] = wins.get(visitor_team, 0) + 1
+            else:
+                winner = "Draw"
+
+            games.append({
+                "date": game["GAME_DATE"],
+                "home_team": home_team,
+                "visitor_team": visitor_team,
+                "visitor_score": visitor_score,
+                "home_score": home_score,
+                "winner": winner,
+            })
+            # next_cursor = data.get("meta", {}).get("next_cursor")
+            # params["cursor"] = next_cursor
+            # if not next_cursor:
+            #     break
 
     winning_stats = dict(sorted(wins.items(), key = lambda x: x[1], reverse = True))
 
